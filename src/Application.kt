ee81a2d399559159848b3fa5edcde1f6e66754e2
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-02-25 21:17:44 +0000

    chore: setup initial project

---
+++ src/Application.kt
@@ -0,0 +1,30 @@
+package com.github
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = true) {
+    install(ContentNegotiation) {
+        gson { }
+
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
